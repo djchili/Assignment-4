af3785b86a54310038cfac9da9f712171bec3ecb
AnthonyVas <89411083+djchili@users.noreply.github.com>
2021-09-10 23:18:41 +0000

    got it working

--- nextjs-blog/components/layout.js
+++ nextjs-blog/components/layout.js
@@ -5,21 +5,22 @@ import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
 const name ='Anthony';
-export const siteTitle = 'Next.js sample website'
+export const siteTitle = 'Next.js Sample Website'
 
-export default function Layout({children, home}) {
+export default function Layout({ children, home }) {
   return (
     <div className={styles.container}>
     <Head>
       <link rel="icon" href="/favicon.ico" />
       <meta
         name="description"
-        content="Learn how to build a personal website using Next.js" />
+        content="Learn how to build a personal website using Next.js" 
+        />
         <meta
           property="og:image"
           content={`https://og-image.vercel.app/${encodeURI(
             siteTitle
-          )}.png?theme=light&md=O&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
           />
           <meta name="og:title" content={siteTitle} />
           <mate name="twitter:card" content="summary_large_image" />
@@ -37,7 +38,7 @@ export default function Layout({children, home}) {
         />
         <h1 className={utilStyles.heading2X1}>{name} </h1>
         </>
-      ) :(
+      ) : (
         <>
           <Link href="/">
             <a>
@@ -63,7 +64,7 @@ export default function Layout({children, home}) {
     {!home && (
       <div className={styles.backToHome}>
       <Link href="/">
-      <a> Back to home  </a>
+      <a> Back to home</a>
       </Link>
       </div>
     )}
--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -1,45 +1,46 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
-import { getSortedPostData } from '../lib/posts'
+import { getSortedPostsData } from '../lib/posts'
+import Link from 'next/link'
+import Date from '../components/date'
 
-export async function getStaticprops() {
-  const allPostsData = getSortedPostData()
-  return {
-    props: {
-      allPostsData
-    }
-  }
-}
-
-export default function Home({ allPostData }) {
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
     <Head>
-    <title>{siteTitle}</title>
+      <title>{siteTitle}</title>
     </Head>
-    <section className={utilStyles.headingMd}>
-    <p>Trying to figure it out!</p>
-    <p>
-      (This is a sample website - you'll be building a site like this on{' '}
-      <a href= "https://nextjs.org/learn">our Next.js tutorial</a>.)
-    </p>
-    </section> 
+      <section className={utilStyles.headingMd}>
+        <p>Trying to figure it out!</p>
     
-    <section className={`${utilStyles.headingMd} ${utilStyles.passing1px}`}>
+      </section> 
+    
+    <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
     <h2 className={utilStyles.headingLg}>Blog</h2>
     <ul className={utilStyles.list}>
-      {allPostData.map(({ id, date, title }) => (
+      {allPostsData.map(({ id, date, title }) => (
       <li className={utilStyles.listItem} key={id}>
-      {title}
+          <Link href={`/posts/${id}`}>
+      <a>{title}</a>
+      </Link>
       <br />
-      {id}
-      <br />
-      {date}
+      <small className={utilStyles.lightText}>
+            <Date dateString={date} />
+            </small>
       </li>
       ))}
     </ul>
   </section>
   </Layout>
   )
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
 }
