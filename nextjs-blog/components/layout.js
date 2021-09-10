3bdfd1c2013c2fca5cfd6bf30a692451599066d2
AnthonyVas <89411083+djchili@users.noreply.github.com>
2021-09-10 23:46:15 +0000

    mate to meta

--- nextjs-blog/components/layout.js
+++ nextjs-blog/components/layout.js
@@ -1,10 +1,10 @@
-import Head from 'next/head';
-import Image from 'next/image';
+import Head from 'next/head'
+import Image from 'next/image'
 import styles from './layout.module.css'
-import utilStyles from '../styles/utils.module.css';
-import Link from 'next/link';
+import utilStyles from '../styles/utils.module.css'
+import Link from 'next/link'
 
-const name ='Anthony';
+const name ='Anthony'
 export const siteTitle = 'Next.js Sample Website'
 
 export default function Layout({ children, home }) {
@@ -13,7 +13,7 @@ export default function Layout({ children, home }) {
     <Head>
       <link rel="icon" href="/favicon.ico" />
       <meta
-        name="description"
+         name="description"
         content="Learn how to build a personal website using Next.js" 
         />
         <meta
@@ -23,7 +23,7 @@ export default function Layout({ children, home }) {
           )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
           />
           <meta name="og:title" content={siteTitle} />
-          <mate name="twitter:card" content="summary_large_image" />
+          <mata name="twitter:card" content="summary_large_image" />
     </Head>
     <header className={styles.header}>
       {home ? (
