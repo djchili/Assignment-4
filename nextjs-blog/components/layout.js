bf8ea3dd1e90804b4063e5468c75ae17f438e88d
AnthonyVas <89411083+djchili@users.noreply.github.com>
2021-09-11 00:40:46 +0000

    fix layout

--- nextjs-blog/components/layout.js
+++ nextjs-blog/components/layout.js
@@ -23,7 +23,7 @@ export default function Layout({ children, home }) {
           )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
           />
           <meta name="og:title" content={siteTitle} />
-          <mata name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:card" content="summary_large_image" />
     </Head>
     <header className={styles.header}>
       {home ? (
@@ -36,7 +36,7 @@ export default function Layout({ children, home }) {
           width={144}
           alt={name}
         />
-        <h1 className={utilStyles.heading2X1}>{name}</h1>
+        <h1 className={utilStyles.heading2Xl}>{name}</h1>
         </>
       ) : (
         <>
